2731271584647a2b4a66ab9b7da04b1d975def86
Ludovic Siband <ludovic.siband@gmail.com>
2025-03-04 16:17:55 +0100

    :building_construction: build: initialize backend project with Spring Boot and Maven setup

---
+++ src/main/java/com/splitngo/backend/SplitNGoBackApplication.java
@@ -0,0 +1,13 @@
+package com.splitngo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitNGoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitNGoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitngo/backend/SplitNGoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitngo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitNGoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
