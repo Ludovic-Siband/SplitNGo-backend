deca9fda6bf84e29387600b02b8c64bb5a954467
Ludovic Siband <ludovic.siband@gmail.com>
2025-04-15 12:31:03 +0200

    feat: init TripDTO.java

---
+++ src/main/java/com/splitngo/backend/dtos/TripDTO.java
@@ -0,0 +1,18 @@
+package com.splitngo.backend.dtos;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TripDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime dateStart;
+    private LocalDateTime dateEnd;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private boolean isActive;
+}
